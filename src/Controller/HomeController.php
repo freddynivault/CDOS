fef5486998a519e5c2f2321f56cdb956d55432d3
freddynivault <freddy.nivault@etu.univ-poitiers.fr>
2022-01-18 15:55:38 +0100

    commit pour bouton se connecter et créer un compte dans nav bar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -23,4 +23,22 @@ class HomeController extends AbstractController
     {
         return $this->render ('home/listoffer.html.twig');
     }
+
+    /**
+     * @Route("/createaccount", name="app_createaccount")
+     */
+    public function createaccount(): Response
+    {
+        return $this->render ('home/createaccount.html.twig');
+    }
+
+    /**
+     * @Route("/connect", name="app_connect")
+     */
+    public function connect(): Response
+    {
+        return $this->render ('home/connect.html.twig');
+    }
+
+
 }
