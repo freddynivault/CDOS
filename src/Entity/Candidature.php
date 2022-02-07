07701c7843c952bfae0882ba8c6d55c5dd7d8c50
ThibaultMeekel <thibault.meekel.tm@gmail.com>
2022-02-07 10:36:17 +0100

    add candidature

---
+++ src/Entity/Candidature.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CandidatureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CandidatureRepository::class)
+ */
+class Candidature
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $cv;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lettreMotivation;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $commentaire;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Upload")
+     */
+    private $offer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCv(): ?string
+    {
+        return $this->cv;
+    }
+
+    public function setCv(string $cv): self
+    {
+        $this->cv = $cv;
+
+        return $this;
+    }
+
+    public function getLettreMotivation(): ?string
+    {
+        return $this->lettreMotivation;
+    }
+
+    public function setLettreMotivation(string $lettreMotivation): self
+    {
+        $this->lettreMotivation = $lettreMotivation;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOffer()
+    {
+        return $this->offer;
+    }
+
+    /**
+     * @param mixed $offer
+     */
+    public function setOffer($offer): void
+    {
+        $this->offer = $offer;
+    }
+}
